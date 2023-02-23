6fc25c5b8ed375ff91975ac0177bf41b84648089
Tobias Königs <106632261+lmbfxn@users.noreply.github.com>
2023-02-23 18:40:33 +0100

    Added NextAuth.js File with Discord Provider

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,12 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import DiscordProvider from "next-auth/providers/discord"
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID || "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
+    })
+  ]
+}
+export default NextAuth(authOptions)
